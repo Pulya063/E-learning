995391d368108e8d441b5f70017a7dfb9054906b
Pulya063 <leatherstar07@gmail.com>
2026-02-05 22:34:24 +0100

    added wraps

--- common/other.py
+++ common/other.py
@@ -1,8 +1,11 @@
+from functools import wraps
+
 from django.contrib import messages
 from django.shortcuts import redirect
 
 def check_user_group(group_name):
     def decorator(view_func):
+        @wraps(view_func)
         def wrapper(request, *args, **kwargs):
             if request.user.groups.filter(name=group_name).exists():
                 return view_func(request, *args, **kwargs)
